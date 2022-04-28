0287b645395c3ac776f36ed18e8f5d90e9ba7b01
Antonio Robledo <antonio.robledo@askblue.pt>
2022-04-28 19:21:38 +0100

    DB

---
+++ SmartMedDB/dbo/Tables/TbMedication.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[TbMedication] (
+    [Id]         INT          IDENTITY (1, 1) NOT NULL,
+    [Name]       VARCHAR (50) NULL,
+    [quantity]   INT          NULL,
+    [CreateDate] DATETIME     NULL
+);
+
